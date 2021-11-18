7bdab8653a79548bb3e0ad01ef3dbf22c483f339
Steven Jennings <8878289+srjennings@users.noreply.github.com>
2021-11-18 08:31:15 -0500

    UTF-8 without BOM

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,23 +1,23 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.microsoft.com/download/6/1/C/61CC0718-ED0E-4351-BC54-46495EBF5CC3/iiscompression_x86.msi'
-$url64      = 'https://download.microsoft.com/download/6/1/C/61CC0718-ED0E-4351-BC54-46495EBF5CC3/iiscompression_amd64.msi'
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://download.microsoft.com/download/6/1/C/61CC0718-ED0E-4351-BC54-46495EBF5CC3/iiscompression_x86.msi'
+$url64 = 'https://download.microsoft.com/download/6/1/C/61CC0718-ED0E-4351-BC54-46495EBF5CC3/iiscompression_amd64.msi'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'MSI'
-  url           = $url
-  url64bit      = $url64
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'MSI'
+  url            = $url
+  url64bit       = $url64
 
-  softwareName  = 'iis-compression*'
+  softwareName   = 'iis-compression*'
 
-  checksum      = 'f405536c17d3082d28641ef22978081395772c91c51f4279982f5ee069eae1b1'
-  checksum64    = '701aae3ff0078dcf263bd35cb53f866c4b0160036af940deda4bef1ae1a816b6'
+  checksum       = 'f405536c17d3082d28641ef22978081395772c91c51f4279982f5ee069eae1b1'
+  checksum64     = '701aae3ff0078dcf263bd35cb53f866c4b0160036af940deda4bef1ae1a816b6'
 
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
-  validExitCodes= @(0, 3010, 1641)
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs
