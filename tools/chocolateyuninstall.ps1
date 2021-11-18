d1e2527c357880678817d94fe5ea8c54cc8031dd
Steven Jennings <8878289+srjennings@users.noreply.github.com>
2021-11-18 08:31:37 -0500

    UTF-8 without BOM

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -1,22 +1,22 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = 'iis-compression*'
-  fileType      = 'MSI'
-  silentArgs    = "/qn /norestart"
-  validExitCodes= @(0, 3010, 1605, 1614, 1641)
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'iis-compression*'
+  fileType       = 'MSI'
+  silentArgs     = "/qn /norestart"
+  validExitCodes = @(0, 3010, 1605, 1614, 1641)
 }
 
 $uninstalled = $false
 [array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
 
 if ($key.Count -eq 1) {
-  $key | % { 
+  $key | ForEach-Object {
     $packageArgs['file'] = "$($_.UninstallString)"
-    
+
     if ($packageArgs['fileType'] -eq 'MSI') {
       $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
-      
+
       $packageArgs['file'] = ''
     } else {
     }
@@ -29,5 +29,5 @@ if ($key.Count -eq 1) {
   Write-Warning "$($key.Count) matches found!"
   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
   Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $($_.DisplayName)"}
+  $key | ForEach-Object { Write-Warning "- $($_.DisplayName)" }
 }
